8744574536d3a590076bfc8d38705b41bd9cdf56
Dan Wu <Wu@gmail.com>
2024-03-16 14:33:07 +0800

    initial setting

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 8800;
+const mongoose = require("mongoose");
+
+app.get("/", (req, res) => {
+  res.send("welcome to index");
+});
+
+app.listen(port, () => {
+  console.log("server is listening on port 8800");
+});
