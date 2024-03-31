e32424e95a8b1f16dde5dbd11a674969b301c178
Dan Wu <Wu@gmail.com>
2024-03-31 16:08:01 +0800

    using swiper to show slide

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const port = 8800;
 const mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 let myKey = "975a07d0fdf4f9abd01458d8f1f9f9c5";
 
 let introUrl = `https://api.themoviedb.org/3/movie/157336?api_key=${myKey}&language=zh-TW`;
 let videosUrl = `https://api.themoviedb.org/3/movie/157336/videos?api_key=${myKey}`;
 let languageUrl = "https://api.themoviedb.org/3/tv/1399?language=zh-TW";
+const releasedMovieUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${myKey}&language=zh-TW`;
 
 // 預告片API
 async function videos() {
@@ -24,9 +26,9 @@ async function videos() {
 videos();
 
 // 電影簡介API
-async function movieIntro() {
+async function movieIntro(url) {
   try {
-    let result = await fetch(introUrl);
+    let result = await fetch(url);
     let data = await result.json();
     console.log(data);
     return data;
@@ -35,8 +37,14 @@ async function movieIntro() {
   }
 }
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  try {
+    let releasedMovie = await movieIntro(releasedMovieUrl);
+    console.log(releasedMovie.results);
+    res.render("index", { releasedMovieData: releasedMovie.results });
+  } catch (e) {
+    return res.status(500).send(e);
+  }
 });
 
 app.get("/moiveIntro/:movie_id", async (req, res) => {
